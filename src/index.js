c561da2e43d6c78eeeb0e8649cd5f5e29ed8857d
Niklas Mischkulnig <mischnic@users.noreply.github.com>
2020-12-17 20:00:46 +0100

    Bundle less

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import {
 	StreamReport,
 } from "@yarnpkg/core";
 import NpmPlugin from "@yarnpkg/plugin-npm";
-import GitPlugin from "@yarnpkg/plugin-git";
-import GithubPlugin from "@yarnpkg/plugin-github";
+// import GitPlugin from "@yarnpkg/plugin-git";
+// import GithubPlugin from "@yarnpkg/plugin-github";
 import NodeModulesPlugin from "@yarnpkg/plugin-node-modules";
 
 import { Writable } from "stream";
@@ -17,73 +17,73 @@ export const memfs = _memfs;
 
 import fakeFS from "./fs.cjs";
 
-class MyReport extends Report {
-	reportCacheHit(locator) {}
-	reportCacheMiss(locator) {}
-	startTimerSync(what, opts, cb) {
-		const realCb = typeof opts === `function` ? opts : cb;
-		return realCb();
-	}
-	async startTimerPromise(what, opts, cb) {
-		const realCb = typeof opts === `function` ? opts : cb;
-		return await realCb();
-	}
-	async startCacheReport(cb) {
-		return await cb();
-	}
-	reportSeparator() {}
-	reportInfo(name, text) {
-		console.log("reportInfo", name, text);
-	}
-	reportWarning(name, text) {
-		console.log("reportWarning", name, text);
-	}
-	reportError(name, text) {
-		console.log("reportError", name, text);
-	}
-	reportProgress(progress) {
-		const promise = Promise.resolve().then(async () => {
-			for await (const p of progress) {
-				console.log("reportProgress", p);
-			}
-		});
+// class MyReport extends Report {
+// 	reportCacheHit(locator) {}
+// 	reportCacheMiss(locator) {}
+// 	startTimerSync(what, opts, cb) {
+// 		const realCb = typeof opts === `function` ? opts : cb;
+// 		return realCb();
+// 	}
+// 	async startTimerPromise(what, opts, cb) {
+// 		const realCb = typeof opts === `function` ? opts : cb;
+// 		return await realCb();
+// 	}
+// 	async startCacheReport(cb) {
+// 		return await cb();
+// 	}
+// 	reportSeparator() {}
+// 	reportInfo(name, text) {
+// 		console.log("reportInfo", name, text);
+// 	}
+// 	reportWarning(name, text) {
+// 		console.log("reportWarning", name, text);
+// 	}
+// 	reportError(name, text) {
+// 		console.log("reportError", name, text);
+// 	}
+// 	reportProgress(progress) {
+// 		const promise = Promise.resolve().then(async () => {
+// 			for await (const p of progress) {
+// 				console.log("reportProgress", p);
+// 			}
+// 		});
 
-		const stop = () => {};
+// 		const stop = () => {};
 
-		return { ...promise, stop };
-	}
+// 		return { ...promise, stop };
+// 	}
 
-	reportJson(data) {
-		console.log("reportJson", data);
-	}
-	async finalize() {}
-}
+// 	reportJson(data) {
+// 		console.log("reportJson", data);
+// 	}
+// 	async finalize() {}
+// }
 
 class StringStream extends Writable {
 	constructor() {
 		super();
 		this.chunks = [];
 	}
-	toString() {
-		return Buffer.concat(this.chunks).toString("utf8");
+	getChunks() {
+		return this.chunks;
 	}
 	_write(chunk, enc, next) {
-		this.chunks.push(chunk);
+		this.chunks.push(JSON.parse(chunk.toString()));
 		next();
 	}
 }
 
 const plugins = new Map([
+	// [`@yarnpkg/plugin-git`, GitPlugin.default ?? GitPlugin],
+	// [`@yarnpkg/plugin-github`, GithubPlugin.default ?? GithubPlugin],
 	[`@yarnpkg/plugin-npm`, NpmPlugin.default ?? NpmPlugin],
-	[`@yarnpkg/plugin-git`, GitPlugin.default ?? GitPlugin],
-	[`@yarnpkg/plugin-github`, GithubPlugin.default ?? GithubPlugin],
 	[
 		`@yarnpkg/plugin-node-modules`,
 		NodeModulesPlugin.default ?? NodeModulesPlugin,
 	],
 ]);
 
-export async function run({ fs, dir }) {
+export async function run({ fs, dir, options = {} }) {
 	fakeFS.__override(fs);
 
 	const configuration = Configuration.create(dir, dir, plugins);
@@ -92,6 +92,8 @@ export async function run({ fs, dir }) {
 		{
 			nodeLinker: "node-modules",
 			npmRegistryServer: "https://registry.npmjs.cf",
+			compressionLevel: 0,
+			...options,
 		},
 		dir
 	);
@@ -100,10 +102,10 @@ export async function run({ fs, dir }) {
 
 	const stdout = new StringStream();
 
-	await StreamReport.start(
+	let report = await StreamReport.start(
 		{
 			configuration,
-			json: false,
+			json: true,
 			stdout,
 			includeLogs: true,
 		},
@@ -112,30 +114,6 @@ export async function run({ fs, dir }) {
 		}
 	);
 
-	console.log(stdout.toString());
+	// TODO stream into a callback
+	return { report, messages: stdout.getChunks() };
 }
-
-// import { tgzUtils, structUtils } from "@yarnpkg/core";
-// import { Buffer } from "buffer";
-
-// import fs from "memfs";
-
-// export default async ({ /*fs, */ dir }) => {
-// 	// fakeFS.__override(fs);
-// 	fs.mkdirSync("/tmp");
-
-// 	let sourceBuffer = Buffer.from(
-// 		"H4sIAAAAAAAAA+1W32vbMBDus/+KIxk0gdV2bCeG7geDvaxlG2PpnkpGNftSq7EtT5LThtL/fScpSfMwyGBtYMzfi9B3p7uTdKdTw7IFu8agcaN/o0R99MQIw3CSJGDGdDK2Yxi5ucEkDuFoFMXjME7SZJIchaPYyZ86kN+hVZpJCuVv7bi9wHb8R3DvAfRqVmHvFHqZUicNkwp7Lw29RKm4qI0k8kM/dGyOKpO80WvJ++kU7BrpxBXjlud1jneOmvMSFXGXNIG1hBKtR9PZ2mSDRNYZt3r3To+iMYa+O9/EPVhl1upCSCO5OIcPohS3bJUV7QJe65t3S24iPqkw58zP2FsXQMkzrJXd4qezC8dJbITiWsjVo0e9aqzSNddWiahWloYptG7UaRCQpGh/+JmoAom3Qi4oyGB7bL5ZuA10gSvSyHd2bna0trtzZDRTekVHVCDa5TPvwTvU/a/rPthcynP42FP/YZLa+k/idBTGaUj1H43ScVf/h0Al8rZEH+8aIbWCNyDxZ8slDo4pWY+Hvs3TVwdLxw4Hxqb+vyLLK/Sr/Bl87Kn/JIp26n9s6z9Koq7+D4E+bLuXR63nnC3Z1LZ3eGzsMBcSPovc/A5hsHkqdIFwZRWuoELqyTmIOVySudlgX7scDj2v34ezmg6/LJn5SxjvL6BuKuCO3Q2MdL8pSlOj9KVEphAUog3hDx2Ce+ggF1lbYa2tT99a/uh+B8Y2/Q66p65Dhw7/CX4BBXc2ugAQAAA",
-// 		"base64"
-// 	);
-
-// 	let zipFs = await tgzUtils.convertToZip(sourceBuffer, {
-// 		compressionLevel: "mixed",
-// 		prefixPath: "node_modules/lodash-es",
-// 		stripComponents: 1,
-// 	});
-// 	console.log(zipFs);
-// 	console.log(zipFs.getRealPath());
-// 	zipFs.saveAndClose();
-// 	console.log(fs.readdirSync("/tmp/" + fs.readdirSync("/tmp")[0]));
-// };
